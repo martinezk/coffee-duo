e84e9a70f599e604c1a63daa8220d4ffff9e7b9b
Kristina Martinez <misskristinamartinez@gmail.com>
2017-08-29 13:20:58 -0400

    started pairing quiz files

---
+++ src/PairingQuizComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Question from './Question';
+
+class PairingQuizComp extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default PairingQuizComp;
