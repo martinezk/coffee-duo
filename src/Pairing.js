3db9d2095f81b2465e58a3e2d29d5bdcd973643f
Kristina Martinez <misskristinamartinez@gmail.com>
2017-10-12 21:12:24 -0400

    setting up pairing component

--- src/Pairing.js
+++ src/Pairing.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; 
+import { connect } from 'react-redux';
+import { itemsFetchData } from './actions/quiz-actions';
+import { items } from './actions/quiz-actions';
 
 class Pairing extends React.Component {
+  componentDidMount(){
+    this.props.fetchData('/api/json/Drinks.json');
+  }  
 
-  render() {
+  render(){
+    if (this.props.hasErrored) {
+      return <p>Error loading items</p>;
+    }
+    if (this.props.isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
-      <div>
-        <h3>Hello</h3>
-      </div>
+      <ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>
+            {item.Item}
+          </li>
+        ))}
+      </ul>
     );
   }
 }
-export default Pairing;
+
+const mapStateToProps= (state) =>{
+  return {
+    items: state.items,
+    hasErrored: state.itemsHasErrored,
+    isLoading: state.itemsIsLoading
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    fetchData: (url) => dispatch(itemsFetchData(url))
+  };
+};
+
+Pairing.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  hasErrored: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Pairing);
