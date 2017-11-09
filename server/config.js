2703f8d7ffcf6328f24a0fe8bef320bb1130eacf
Kristina Martinez <misskristinamartinez@gmail.com>
2017-11-09 07:04:00 -0500

    setup proxy server

--- server/config.js
+++ server/config.js
@@ -3,4 +3,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL ||
 'mongodb://java:password1@ds243335.mlab.com:43335/coffeeduo';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3001;
